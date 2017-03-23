a20de65a8905b589f32da45b435d6ae98c62d090
akaidc2 <alex.kaidan@wendys.com>
2017-03-23 17:15:55 -0400

    nearly complete with basics for calc app.

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,14 +1,23 @@
 import React from 'react';
 import { graphql } from 'react-apollo';
 import gql from 'graphql-tag';
-import FuseTestContainer from './components/FuseTestContainer.jsx'
+import RoommateCalc from './components/RoommateCalc.jsx'
+
+import injectTapEventPlugin from 'react-tap-event-plugin';
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
 
 // Or a stateless functional component:
-const App = ({data}) => (
-    <div>
-      {data.say}
-        <FuseTestContainer />
-    </div>
+const App = ({/*data*/}) => (
+    <MuiThemeProvider>
+        <div>
+          {/*data.say*/}
+            <RoommateCalc />
+        </div>
+    </MuiThemeProvider>
   );
 
 // Initialize GraphQL queries or mutations with the `gql` tag
