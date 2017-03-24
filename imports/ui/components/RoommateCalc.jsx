76166869b5878edf17d289dc158677d39376256e
akaidc2 <alex.kaidan@wendys.com>
2017-03-24 00:48:06 -0400

    Name and calc logic now working as planned.

--- imports/ui/components/RoommateCalc.jsx
+++ imports/ui/components/RoommateCalc.jsx
@@ -12,11 +12,7 @@ class FuseTestContainer extends React.Component {
         super();
 
         this.state = {
-            roomies: [{
-                name: 'Name',
-                daysInRoom: 'Days In Room',
-                amountOwed: 'Amount Owed'
-            }],
+            roomies: [],
             daysRented: 1,
             sumOfDaysRented: 1,
             costPerNight: 100
@@ -49,9 +45,11 @@ class FuseTestContainer extends React.Component {
                         </div>
                         <p className="roomie-header">3. Add some roomies and select the number of days they'll be in the room</p>
                         <div className="roomies-container">
+                            <RoomieComp key={`roomie-header`} roomie={{name: 'Name',daysInRoom: 'Days In Room',amountOwed: 'Amount Owed'}} idx={-1}
+                                        onAdd={()=>{this.addRoomie(-1)}} onRemove={()=>{this.removeRoomie(-1)}}/>
                             {this.state.roomies.map((roomie, idx)=> {
-                                return <RoomieComp key={`roomie-${idx}`} roomie={roomie} idx={idx}
-                                                   onAdd={()=>{this.addRoomie(idx)}} onRemove={()=>{this.removeRoomie(idx)}}/>
+                                return <RoomieComp key={`roomie-${idx}`} roomie={roomie} idx={idx} nameOnChange={this.setRoomieName}
+                                                   onAdd={()=>{this.addRoomie(idx)}} onRemove={()=>{this.removeRoomie(idx)}} />
                             })}
                         </div>
                     </div>
@@ -70,30 +68,36 @@ class FuseTestContainer extends React.Component {
             this.setState({costPerNight})
         }
     }
+    setRoomieName = (e,idx) => {
+        const roomieName = e.currentTarget.value;
+        const roomiesClone = _.cloneDeep(this.state.roomies);
+
+        roomiesClone[idx].name = roomieName //assumes idx exists because made from array
+
+        this.setState({roomies: roomiesClone})
+    }
     addRoomie = (idx) => {
         const self= this;
         const currentRoomies = _.cloneDeep(this.state.roomies)
 
         const newStudent = {
-            name: 'New roomie',
+            name: "New roomie",
             daysInRoom: 1,
             amountOwed: 1
         }
-
-        currentRoomies.splice(++idx,0,newStudent);
+        currentRoomies.splice(idx+1,0,newStudent);
 
         this.setState({roomies:currentRoomies}, ()=>{
-            $('select').material_select();
             self.recalcCost();
         })
     }
     removeRoomie = (idx) => {
+        const self = this;
         const currentRoomies = _.cloneDeep(this.state.roomies);
 
         currentRoomies.splice(idx, 1)
 
         this.setState({roomies:currentRoomies}, ()=>{
-            $('select').material_select();
             self.recalcCost();
         })
     }
@@ -102,23 +106,20 @@ class FuseTestContainer extends React.Component {
         const totalCost = this.state.daysRented * this.state.costPerNight;
 
         let sumOfDaysRented = 0;
-        roomiesClone.forEach((roomie,idx)=>{
-            if(idx != 0)
+        roomiesClone.forEach((roomie)=>{
             sumOfDaysRented +=roomie.daysInRoom;
         })
 
         roomiesClone.forEach((roomie, idx)=>{
-            if(idx != 0) {
-                const amountOwed = (roomie.daysInRoom / sumOfDaysRented * totalCost).toFixed(2)
-                roomie.amountOwed = amountOwed
-            }
+            const amountOwed = (roomie.daysInRoom / sumOfDaysRented * totalCost).toFixed(2)
+            roomie.amountOwed = amountOwed
         })
 
         this.setState({roomies: roomiesClone})
     }
 }
 
-function RoomieComp ({roomie, idx, onAdd, onRemove}) {
+function RoomieComp ({roomie, idx, onAdd, onRemove, nameOnChange}) {
     const optionsForDays = []
     for(let a = 1; a < 11; a++){
         optionsForDays.push(<MenuItem key={`daysOptions-${a}`} value={a} primaryText={a} />)
@@ -129,14 +130,14 @@ function RoomieComp ({roomie, idx, onAdd, onRemove}) {
                         </SelectField></div>
 
     return (
-        <div className={`row valign-wrapper roomie ${idx==0? 'first-row': ''}` }>
-            <div className="col s4">{idx==0?roomie.name : <TextField id={`nameField-${idx}`} defaultValue={roomie.name} fullWidth={true}/>}</div>
-            <div className="col s3">{idx==0?roomie.daysInRoom: daySelector}</div>
-            <div className="col s3">{(idx!==0?'$':'')+roomie.amountOwed}</div>
-            <div className={`col s1 icon remove ${idx==0? 'hide': ''}`}>
-                <button onClick={onRemove} className="btn waves-effect waves-light light-blue darken-2">-</button>
+        <div className={`row valign-wrapper roomie ${idx<0? 'first-row': ''}` }>
+            <div className="col s4">{idx <0?roomie.name : <TextField onChange={(e)=>{nameOnChange(e,idx)}} id={`nameField-${idx}`} value={roomie.name} fullWidth={true}/>}</div>
+            <div className="col s3">{idx <0?roomie.daysInRoom: daySelector}</div>
+            <div className="col s3">{(idx >=0?'$':'')+roomie.amountOwed}</div>
+            <div className={`col s1 icon remove ${idx <0? 'hide': ''}`}>
+                <button onClick={()=>{onRemove(idx)}} className="btn waves-effect waves-light light-blue darken-2">-</button>
             </div>
-            <div className="col s1 icon add"><button onClick={onAdd} className="btn waves-effect waves-light light-blue darken-2">+</button></div>
+            <div className="col s1 icon add"><button onClick={()=>{onAdd(idx)}} className="btn waves-effect waves-light light-blue darken-2">+</button></div>
         </div>
     )
 }
