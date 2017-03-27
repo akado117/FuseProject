6c37e1e056a9462f2221533de63d747dfef3fbe6
akaidc2 <alex.kaidan@wendys.com>
2017-03-27 16:37:09 -0400

    graphql now working and /graphiql now working

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { graphql, gql } from 'react-apollo';
 import RoommateCalc from './components/RoommateCalc.jsx'
+const test = gql;
 
 import injectTapEventPlugin from 'react-tap-event-plugin';
 // Needed for onTouchTap
@@ -20,12 +21,15 @@ const App = ({/*data*/}) => (
   );
 
 // Initialize GraphQL queries or mutations with the `gql` tag
-//const query = gql`query { say{} }`;
+const query = gql`query {say {
+    foo
+    bar
+  }}`;
 //const MyMutation = gql`mutation MyMutation { addTodo(text: "Test 123") { id } }`;
 
 // We then can use `graphql` to pass the query results returned by MyQuery
 // to MyComponent as a prop (and update them as the results change)
-export default App//graphql(query)(App);
+export default graphql(query)(App);
 
 // Or, we can bind the execution of MyMutation to a prop
 // export default graphql(MyMutation)(MyComponent);
