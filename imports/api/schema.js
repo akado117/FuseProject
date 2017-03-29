24ab88c2565807f046929ff5a55edfa4f2274f8d
akaidc2 <alex.kaidan@wendys.com>
2017-03-29 18:21:36 -0400

    added in first working example of Graphql interface

--- imports/api/schema.js
+++ imports/api/schema.js
@@ -1,5 +1,10 @@
 export default [
   `
+  scalar Date
+  scalar UUID
+  scalar JSON
+  scalar Coord
+
   type Url {
     url: String!
   }
@@ -7,6 +12,23 @@ export default [
   type Test {
     foo: String,
     bar: String
+  }
+
+  type AvatarUrls {
+    i48x48: String,
+    i24x24: String,
+    i16x16: String,
+    i32x32: String
+  }
+
+  type Project {
+    expand: String,
+    self: String,
+    id: Int,
+    key: String,
+    name: String,
+    avatarUrls: AvatarUrls,
+    projectTypeKey: String
   }
 
   type RootMutation {
@@ -17,10 +39,12 @@ export default [
     say: Test
     says: [Test]
     urls: [Url]
+    getAllProjects: [Project]
   }
 
   schema {
     query: RootQuery
     mutation: RootMutation
-  }`,
+  }`
 ];
+
